ed53a773c0b4578819394e230146edd0f1542cf3
jhays200 <jonathan.hays@oit.edu>
2011-05-19 03:15:18 -0700

    Added CMakeLists.txt for RCPython and made Velo Communication work

--- src/RCPython/VeloComm.cpp
+++ src/RCPython/VeloComm.cpp
@@ -1,24 +1,27 @@
 #include "VeloComm.hpp"
 #include <unistd.h>
 #include <string>
-#include <iostream>
+#include <cstdio>
 
 using namespace std;
 using std::string;
 
+const int PYSCRIPT_PATH_LEN = 100;
+const int COMMAND_LEN = 30;
+
 VeloComm::VeloComm(): pModule(0)
 {
 	string command = "sys.path.append(\"";
-	char cwdPath[100];
+	char cwdPath[PYSCRIPT_PATH_LEN];
 	int i = 0;
 	
 	//getcwd and store it into cwdPath
 	
 	//create a command to add the script to the path
-	getcwd(cwdPath, 100);
+	getcwd(cwdPath, PYSCRIPT_PATH_LEN); //get current working directory path
 	command.append(cwdPath);
 	command.append("\")\n");
-	cout << command << endl;
+	printf(command.c_str());
 	
 	Py_Initialize();
 	
@@ -26,13 +29,8 @@ VeloComm::VeloComm(): pModule(0)
 	PyRun_SimpleString("import sys\n");
 	PyRun_SimpleString(command.c_str());
 	PyRun_SimpleString("from VeloComm import Test, Init\n");
-	PyRun_SimpleString("Init(5)\n");
-	PyRun_SimpleString("Test(5)\n");
-	//PyRun_SimpleString("time.sleep(100)");
-	PyRun_SimpleString("Test(3)\n");
-	//PyRun_SimpleString("time.sleep(100)");
-	PyRun_SimpleString("Test(5)\n");
-	//PyRun_SimpleString("time.sleep(100)");
+	PyRun_SimpleString("Init()\n");
+	
 }
 	
 
@@ -43,10 +41,15 @@ VeloComm::~VeloComm()
 
 void VeloComm::Test(double value)
 {
+	char command[COMMAND_LEN];
+	sprintf(command, "Test(%f)\n", value);
+	PyRun_SimpleString(command);
 }
 
 int main()
 {
 	VeloComm * test = new VeloComm;
+	for(int i = 32; i != 0; --i)
+		test->Test((double)i);
 	delete test;
 }
